2a15b02d14d0e08554e415dc7fb087cdce430190
Jasper Wu <jasper.wu@cargosmart.com>
2018-03-08 13:35:19 +0800

    Add IntTest for NoteController using hamcrest lib.

--- src/test/java/com/jpuneng/springboot_dom_notebook/SpringbootDomNotebookApplicationTests.java
+++ src/test/java/com/jpuneng/springboot_dom_notebook/SpringbootDomNotebookApplicationTests.java
@@ -1,16 +1,40 @@
 package com.jpuneng.springboot_dom_notebook;
 
+import com.jpuneng.springboot_dom_notebook.controller.note.NoteReadController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class SpringbootDomNotebookApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MockMvc mockMvc;
+	@Autowired
+	NoteReadController controller;
+
+	@Before
+	public void setUp(){
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
 
+	@Test
+	public void contextLoads() throws Exception {
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/note/GET/list/0").accept(MediaType.APPLICATION_JSON));
+		resultActions.andExpect(status().isOk()).andExpect(content().string(startsWith("{\"code\":200,\"message\":null,\"data\":[{")));
+	}
 }
